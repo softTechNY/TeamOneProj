473f61b1539302a829faf2976fd93a70a261d140
mdhossain62 <mhossain456@gmail.com>
2017-01-14 12:24:09 -0500

    Drop Down Methods

--- BedBathBeyond/src/test/java/dropDown/DropDown.java
+++ BedBathBeyond/src/test/java/dropDown/DropDown.java
@@ -1,11 +1,30 @@
 package dropDown;
 
+import PageFactorySearch.DropDownMenuPage;
 import core.DropdownMethods;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
 
 /**
  * Created by JHANGIR on 1/7/2017.
  */
-public class DropDown extends DropdownMethods {
+public class DropDown extends DropDownMenuPage {
+    @Test
+    public void Dropdown() throws NullPointerException, InterruptedException {
+        //  DropdownMethods drop = PageFactory.initElements(driver, DropdownMethods.class);
+        iterate();
+    }
+
+    public void iterate() {
+        DropdownMethods drop = PageFactory.initElements(driver, DropdownMethods.class);
+        for (int i = 1; i <= 15; i++) {
+            ClickDropDownMenuXpath();
+            clickByXpath(".//*[@id='globalSearchForm']/div[1]/ul/li[" + i + "]/span");
+
+
+        }
+    }
 
 }
+
 
